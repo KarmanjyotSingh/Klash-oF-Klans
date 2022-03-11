9fe9829b7303b8f2cbfc94487be4fb2960b52962
KarmanjyotSingh <karmanjyot.singh@students.iiit.ac.in>
2022-03-11 12:59:16 +0530

    Added spells and attack barb

--- objects/king.py
+++ objects/king.py
@@ -21,29 +21,63 @@ class King(Troop):
         self.movement_speed = macros.KING_MOVEMENT_SPEED
 
     def moveKing(self, char, village):
+
         #  just to check health bar
         if char == '1':
             self.health -= int(1)
-        elif char == 'w':
+
+        if char == 'w':
+
             # is present at the top edge of the game
-            if self.position[0] > 0:
-                if village.tiles[self.position[0]-1][self.position[1]] == macros.EMPTY:
-                    self.position = (self.position[0] - 1, self.position[1])
+            if self.health > 0:
+                if self.position[0] > 0:
+                    steps = 0
+                    for i in range(1, self.movement_speed+1):
+                        if village.tiles[self.position[0]-i][self.position[1]] == macros.EMPTY and self.position[0]-i > 0:
+                            steps += 1
+                        else:
+                            break
+
+                    self.position = (
+                        self.position[0] - steps, self.position[1])
         elif char == 's':
-            if self.position[0] < village.height - 1:
-                if village.tiles[self.position[0]+1][self.position[1]] == macros.EMPTY:
-                    self.position = (self.position[0] + 1, self.position[1])
+            if self.health > 0:
+                if self.position[0] < village.height - 1:
+                    steps = 0
+                    for i in range(1, self.movement_speed+1):
+                        if village.tiles[self.position[0]+i][self.position[1]] == macros.EMPTY and self.position[0]+i < village.height - 1:
+                            steps += 1
+                        else:
+                            break
+                    self.position = (
+                        self.position[0] + steps, self.position[1])
         elif char == 'a':
-            if self.position[1] > 0:
-                if village.tiles[self.position[0]][self.position[1]-1] == macros.EMPTY:
-                    self.position = (self.position[0], self.position[1] - 1)
+            if self.health > 0:
+                if self.position[1] > 0:
+                    steps = 0
+                    for i in range(1, self.movement_speed+1):
+                        if village.tiles[self.position[0]][self.position[1]-i] == macros.EMPTY and self.position[1]-i > 0:
+                            steps += 1
+                        else:
+                            break
+                    self.position = (
+                        self.position[0], self.position[1] - steps)
+
         elif char == 'd':
-            if self.position[1] < village.width - 1:
-                if village.tiles[self.position[0]][self.position[1]+1] == macros.EMPTY:
-                    self.position = (self.position[0], self.position[1] + 1)
-        # elif char == ' ':
+            if self.health > 0:
+                if self.position[1] < village.width - 1:
+                    steps = 0
+                    for i in range(1, self.movement_speed+1):
+                        if village.tiles[self.position[0]][self.position[1]+i] == macros.EMPTY and self.position[1]+i < village.width - 1:
+                            steps += 1
+                        else:
+                            break
+                    self.position = (
+                        self.position[0], self.position[1] + steps)
+
         elif char == ' ':
-            self.attackKing(village)
+            if self.health > 0:
+                self.attackKing(village)
 
         # defining 1 , 2, 3 as spawning points
         elif char == 'z':
@@ -56,6 +90,12 @@ class King(Troop):
             village.barbarians.append(Barabarian(
                 village.spawningPoints[2][0], village.spawningPoints[2][1]))
 
+        elif char == 'r':
+            village.rage.doRage(village)
+
+        elif char == 'h':
+            village.heal.doHeal(village)
+
     def attackKing(self, village):
         # axe vala attack , in radius of 5 tiles
         x = int(self.position[0])
@@ -63,6 +103,7 @@ class King(Troop):
         y = int(self.position[1])
         print(y)
         attackTh = False
+
         for i in range(x-attackRadius, x+attackRadius+1):
             for j in range(y-attackRadius, y+attackRadius+1):
                 # check if it lies inside the box frame
@@ -70,36 +111,35 @@ class King(Troop):
                     # it can be a cannon object , or hut , or townhall , or walls , loop over them
                     if (i, j) in village.coordHut:
                         idx = village.coordHut.index((i, j))
-                        village.huts[idx].health -= self.damage
-                        if village.huts[idx].health <= 0:
-                            village.village[village.huts[idx].position[0]
-                                            ][village.huts[idx].position[1]] = macros.EMPTY
-                            village.huts[idx].texture = macros.BACKGROUND_PIXEL
-                            village.huts[idx].tile = macros.EMPTY
-                            if (village.huts[idx].position[0], village.huts[idx].position[1]) in village.activeBuildings:
-                                village.activeBuildings.remove(
-                                    (village.huts[idx].position[0], village.huts[idx].position[1]))
-
+                        if village.huts[idx].health > 0 and village.huts[idx].active == True:
+                            village.huts[idx].health -= self.damage
+                            if village.huts[idx].health <= 0:
+                                village.village[village.huts[idx].position[0]
+                                                ][village.huts[idx].position[1]] = macros.EMPTY
+                                village.huts[idx].texture = macros.BACKGROUND_PIXEL
+                                village.huts[idx].tile = macros.EMPTY
+                                if (village.huts[idx].position[0], village.huts[idx].position[1]) in village.activeBuildings:
+                                    village.huts[idx].active = False
                     # check for townhall
                     if attackTh == False and i >= macros.COORD_TOWN_HALL[0] and i < macros.COORD_TOWN_HALL[0] + len(village.townhall.drawing) and j >= macros.COORD_TOWN_HALL[1] and j < macros.COORD_TOWN_HALL[1] + len(village.townhall.drawing[0]):
-                        village.townhall.health -= self.damage
+                        if village.townhall.health > 0 and village.townhall.active == True:
+                            village.townhall.health -= self.damage
                         # attack the townhall
-                        if village.townhall.health <= 0:
-                            if (i, j) in village.activeBuildings:
-                                village.activeBuildings.remove((i, j))
-                        attackTh = True
+                            if village.townhall.health <= 0:
+                                village.townhall.active = False
+                            attackTh = True
 
                     # check for cannons
 
                     if(i, j) in village.coordCannon:
                         idx = village.coordCannon.index((i, j))
-                        village.cannons[idx].health -= self.damage
-                        if village.cannons[idx].health <= 0:
-                            village.cannons[idx].texture = macros.BACKGROUND_PIXEL
-                            village.cannons[idx].tile = macros.EMPTY
-                            if (village.cannons[idx].position[0], village.cannons[idx].position[1]) in village.activeBuildings:
-                                village.activeBuildings.remove(
-                                    (village.cannons[idx].position[0], village.cannons[idx].position[1]))
+                        if village.cannons[idx].health > 0 and village.cannons[idx].active == True:
+                            village.cannons[idx].health -= self.damage
+                            if village.cannons[idx].health <= 0:
+                                village.cannons[idx].texture = macros.BACKGROUND_PIXEL
+                                village.cannons[idx].tile = macros.EMPTY
+                                if (village.cannons[idx].position[0], village.cannons[idx].position[1]) in village.activeBuildings:
+                                    village.cannons[idx].active = False
 
                     # check for walls
                     for itr in range(0, len(village.coordWall)):
@@ -111,3 +151,4 @@ class King(Troop):
                             if village.walls[itr].health <= 0:
                                 village.walls[itr].texture = macros.BACKGROUND_PIXEL
                                 village.walls[itr].tile = macros.EMPTY
+                                village.walls[itr].active = False
--- objects/village.py
+++ objects/village.py
@@ -9,6 +9,7 @@ from hut import Hut
 from cannon import Cannon
 from wall import Wall
 from barbarians import Barabarian
+from spell import RageSpell, HealingSpell
 
 
 class Village():
@@ -39,24 +40,35 @@ class Village():
         top = int(macros.VILLAGE_HEIGHT/2)-3
         bottom = int(macros.VILLAGE_HEIGHT/2 + len(self.townhall.drawing)-1)+3
         self.walls = []
+        self.rage = RageSpell()
+        self.heal = HealingSpell()
         for i in range(top, bottom+1):
-            self.coordWall.append((i, left, 2))
-            self.coordWall.append((i, right, 1))
+            if self.coordWall.count((i, left, 1)) == 0:
+                self.coordWall.append((i, left, 1))
+            if self.coordWall.count((i, right, 2)) == 0:
+                self.coordWall.append((i, right, 2))
 
         for j in range(left, right):
-            self.coordWall.append((top, j, 3))
-            self.coordWall.append((bottom, j, 2))
+            if self.coordWall.count((top, j, 1)) == 0:
+                self.coordWall.append((top, j, 2))
+            if self.coordWall.count((bottom, j, 3)) == 0:
+                self.coordWall.append((bottom, j, 3))
+
         left -= 4
         right += 4
         top -= 5
         bottom += 5
         for i in range(top, bottom+1):
-            self.coordWall.append((i, left, 1))
-            self.coordWall.append((i, right, 2))
+            if self.coordWall.count((i, left, 1)) == 0:
+                self.coordWall.append((i, left, 1))
+            if self.coordWall.count((i, right, 2)) == 0:
+                self.coordWall.append((i, right, 2))
 
         for j in range(left, right):
-            self.coordWall.append((top, j, 2))
-            self.coordWall.append((bottom, j, 3))
+            if self.coordWall.count((top, j, 1)) == 0:
+                self.coordWall.append((top, j, 2))
+            if self.coordWall.count((bottom, j, 3)) == 0:
+                self.coordWall.append((bottom, j, 3))
 
         for (x, y, level) in self.coordWall:
             self.walls.append(Wall(x=x, y=y, level=level))
@@ -86,12 +98,11 @@ class Village():
 
         king_health = self.king.health
         max_health = macros.KING_HEALTH_POINTS
-        display_health = int((king_health/max_health) *
-                             macros.DISPLAY_WIDTH-macros.VILLAGE_WIDTH)
+        display_health = math.ceil(float((king_health/max_health) *
+                                         macros.DISPLAY_WIDTH))
 
         for i in range(display_health):
-            self.village[3
-                         ][macros.VILLAGE_WIDTH+i] = Back.RED+' ' + Style.RESET_ALL
+            self.village[0][i] = Back.RED+ " " + Style.RESET_ALL
 
     def drawWalls(self):
         for i in self.walls:
@@ -120,10 +131,20 @@ class Village():
         for row in range(0, len(self.townhall.drawing)):
             for col in range(0, len(self.townhall.drawing[row])):
                 if self.townhall.health > 0:
+                    back = Back.GREEN
+                    health = float(self.townhall.health /
+                                   macros.TOWN_HALL_HEALTH)
+                    if health > 0.5:
+                        back = Back.GREEN
+                    elif health > 0.2:
+                        back = Back.YELLOW
+                    else:
+                        back = Back.LIGHTRED_EX
+
                     self.tiles[macros.COORD_TOWN_HALL[0] +
                                row][macros.COORD_TOWN_HALL[1] + col] = macros.TOWN_HALL
                     self.village[macros.COORD_TOWN_HALL[0]+row][macros.COORD_TOWN_HALL[1] +
-                                                                col] = Back.GREEN + Fore.RED + self.townhall.drawing[row][col] + Style.RESET_ALL
+                                                                col] = back + Fore.RED + self.townhall.drawing[row][col] + Style.RESET_ALL
                 else:
                     self.tiles[macros.COORD_TOWN_HALL[0] +
                                row][macros.COORD_TOWN_HALL[1] + col] = macros.EMPTY
@@ -138,14 +159,46 @@ class Village():
         # render the huts
         itr = 0
         for (x, y) in self.coordHut:
-            self.village[x][y] = self.huts[itr].texture
-            self.tiles[x][y] = self.huts[itr].tile
+            if self.huts[itr].health > 0:
+                self.tiles[x][y] = self.huts[itr].tile
+                health = float(self.huts[itr].health/100)
+                texture = macros.HUT
+                if health > 0.5:
+                    texture = macros.HUT
+                elif health > 0.2:
+                    texture = Back.LIGHTMAGENTA_EX+Fore.BLACK+"H" + Style.RESET_ALL
+                else:
+                    texture = Back.LIGHTRED_EX + Fore.BLACK+"H" + Style.RESET_ALL
+                self.village[x][y] = texture
+
+            else:
+                self.village[x][y] = self.huts[itr].texture
+                self.tiles[x][y] = self.huts[itr].tile
             itr += 1
         itr = 0
         # render the cannons
         for (x, y) in self.coordCannon:
-            self.village[x][y] = self.cannons[itr].texture
-            self.tiles[x][y] = self.cannons[itr].tile
+            if self.cannons[itr].health > 0:
+                # print(str(self.cannons[itr].texture))
+                self.tiles[x][y] = self.cannons[itr].tile
+                health = float(
+                    self.cannons[itr].health/macros.CANNON_HEALTH_POINTS)
+                texture = macros.CANNON
+                if self.cannons[itr].texture == macros.CANNON_SHOT:
+                    print("CANNNON", self.cannons[itr].health)
+                    self.village[x][y] = self.cannons[itr].texture
+                    self.tiles[x][y] = self.cannons[itr].tile
+                else:
+                    if health > 0.5:
+                        texture = macros.CANNON
+                    elif health > 0.2:
+                        texture = macros.midCannon
+                    else:
+                        texture = macros.CannonDead
+                    self.village[x][y] = texture
+            else:
+                self.village[x][y] = self.cannons[itr].texture
+                self.tiles[x][y] = self.cannons[itr].tile
             itr += 1
         # render the spawning points
         for (x, y) in self.spawningPoints:
@@ -153,21 +206,26 @@ class Village():
 
         # render the king
         self.village[self.king.position[0]
-                     ][self.king.position[1]] = macros.KING_TILE
+                     ][self.king.position[1]] = macros.KING_TILE if self.king.health > 0 else macros.GRAVE_TILE
 
         # render barbarians
         for barbarians in self.barbarians:
             self.village[barbarians.position[0]
                          ][barbarians.position[1]] = barbarians.texture
-       
-        # finally draw the village
 
+        # finally draw the village
+        # self.village[21][71] = macros.RED_PIXEL
         for row in range(macros.DISPLAY_HEIGHT):
             for col in range(macros.DISPLAY_WIDTH):
                 print(self.village[row][col],
                       end='\n' if col == macros.DISPLAY_WIDTH-1 else '')
 
+    def shootCannon(self):
+        for cannon in self.cannons:
+            cannon.find_target(self)
+
     def moveBarbs(self):
-        print("Hi")
         for barbs in self.barbarians:
-            barbs.move_barbarian(self)
+            ret = barbs.move_barbarian(self)
+            if ret == True:
+                return True
