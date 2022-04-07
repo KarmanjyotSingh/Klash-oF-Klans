1d673aa009e99e39083c08d287fa8ab87fe3df71
=KarmanjyotSingh <karmanjyot.singh@students.iiit.ac.in>
2022-04-07 17:29:16 +0530

    Added Levels !

--- game.py
+++ game.py
@@ -1,3 +1,29 @@
+from mimetypes import init
+
+from jinja2 import ChoiceLoader
 from src.main import Run
 
-Run()
+
+if __name__ == '__main__':
+
+    print("Welcome to the game!")
+    print("You are Commander of the Village")
+    print("Choose your player : King Or Queen")
+    print("1. King")
+    print("2. Queen")
+    choice = '0'
+
+    while choice != '1' or choice != '2':
+        choice = input()
+        if choice == '1':
+            Run(1)
+            break
+        elif choice == '2':
+            Run(2)
+            break
+        else:
+            print("Invalid choice")
+            print("Please choose 1 or 2")
+            print("1. King")
+            print("2. Queen")
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from src.village import Village
 from src.input import *
 import src.globals as macros
+import src.levels as levels
 
 
-def Run():
-    village = Village()
+def Run(choice):
+    # choice refers to king or queen chosen by the player
+    # choice = 1 for king
+    # choice = 2 for queen
+    village = Village(1, choice)
     village.render()
     inputArr = []
     f = open('src/numreplay.txt', 'r+')
@@ -15,7 +19,11 @@ def Run():
 
         ch = input_to(Get().__call__)
         inputArr.append(ch)
-        ret = village.king.moveKing(ch, village)
+        if choice == 1:
+            village.king.moveKing(ch, village)
+
+        else:
+            village.queen.moveQueen(ch, village)
         if ch == 'q':
             f = open('src/numreplay.txt', 'w')
             f.write(str(n+1))
@@ -31,26 +39,37 @@ def Run():
             village.render()
             village.moveBarbs()
             village.shootCannon()
+            village.shootWizard()
             ret = village.isActive()
             if ret == False:
-                file_name = "replay/" + str(n+1)+".txt"
-                file = open(file_name, 'w')
-                for i in inputArr:
-                    file.write(str(i)+'\n')
-                file.close()
-                f = open('src/numreplay.txt', 'w')
-                f.write(str(n+1))
-                f.close()
-                village.gameWon()
-                break
+                if village.level == 1:
+                    village = Village(2, choice)
+                elif village.level == 2:
+                    village = Village(3, choice)
+                else:
+                    file_name = "replay/" + str(n+1)+".txt"
+                    file = open(file_name, 'w')
+                    for i in inputArr:
+                        file.write(str(i)+'\n')
+                    file.close()
+                    f = open('src/numreplay.txt', 'w')
+                    f.write(str(n+1))
+                    f.close()
+                    village.gameWon()
+                    break
+                    # set the new variables ig
+                # village.campsize = 20
             else:
-                # game lost 
-                if village.campsize == macros.CAMP_SIZE:
-                    flag = False
-                    for barbs in village.barbarians:
-                        flag = barbs.alive
-                    if flag == False and village.king.alive == False:
-                        village.gameLost()
-                        break
+                # game lost
+                flag = False
 
-                
+                for barbs in village.barbarians:
+                    flag = barbs.alive
+                troopFlag = False
+                if choice == 1 : 
+                    troopFlag = village.king.alive
+                else:
+                    troopFlag = village.queen.alive
+                if flag == False and village.campsize == 20 and troopFlag == False:
+                    village.gameLost()
+                    break
